5ac95cbaae3f1a502986c029d0000c77b41d836d
Sebastien Laclau <seb.laclau@gmail.com>
2026-03-31 17:02:00 +0100

    feat: create bootstrap executable

---
+++ crates/bootstrap/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+#[cfg_attr(not(test), panic_handler)]
+#[allow(unused)]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+  loop {
+    hal::halt();
+  }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+  loop {}
+}
